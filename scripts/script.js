5fcb827f8e13636128c0ad15d379ed85d1e712df
Menachem Neiman <nerevar123@gmail.com>
2020-07-02 11:10:50 +0300

    fix: fixed bugs

--- scripts/script.js
+++ scripts/script.js
@@ -6,28 +6,23 @@ let nameInput = document.querySelector('.popup__input_type_name');
 let descInput = document.querySelector('.popup__input_type_desc');
 let nameProfile = document.querySelector('.profile__name');
 let descProfile = document.querySelector('.profile__desc');
+let formElement = document.querySelector('.popup__form');
+
 function showPopup() {
   popup.classList.toggle('popup_opened');
-  nameInput.value = '';
-  descInput.value = '';
-  nameInput.placeholder = nameProfile.textContent;
-  descInput.placeholder = descProfile.textContent;
+  if (popup.classList.contains('popup_opened')) {
+    nameInput.value = nameProfile.textContent;
+    descInput.value = descProfile.textContent;
+  }
 }
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  descProfile.textContent = descInput.value;
+  showPopup();
+}
+
 editButton.addEventListener('click', showPopup);
 closeButton.addEventListener('click', showPopup);
-let formElement = document.querySelector('.popup__form');
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    if (nameInput.value !== '') {
-      nameProfile.textContent = nameInput.value;
-    }else {
-      nameProfile.textContent = nameInput.placeholder;
-    }
-    if (descInput.value !== '') {
-      descProfile.textContent = descInput.value;
-    }else {
-      descProfile.textContent = descInput.placeholder;
-    }
-    showPopup();
-}
 formElement.addEventListener('submit', formSubmitHandler);
